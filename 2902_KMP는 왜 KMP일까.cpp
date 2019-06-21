e955ecce851157b1066913421de9cf08abd949cd
youdongkwan <dkyou7@naver.com>
2019-06-21 11:01:25 +0900

    2902 commit

---
+++ 2902_KMP는 왜 KMP일까.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+
+int main() {
+	string s;
+	cin >> s;
+	vector<char> v;
+	v.push_back(s[0]);
+	for (int i = 1; i < s.size(); i++) {
+		if (s[i] == '-') {
+			v.push_back(s[i + 1]);
+		}
+	}
+	string ans;
+	for (int i = 0; i < v.size(); i++) {
+		ans += v[i];
+	}
+	cout << ans << endl;
+
+	return 0;
+}
